190615ff07cc1981a436812340920ad10300fcef
Samarth <samarthasthana110@gmail.com>
2020-08-22 11:51:30 +0530

    Fourth commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
+import Login from "./Login";
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   const [user, setUser] = useState(null);
 
@@ -11,7 +14,7 @@ function App() {
     <div className="app">
       <Router>
         {!user ? (
-          <h1>Login Page</h1>
+          <Login />
         ) : (
           <>
             <Header />
